f2cf52b7a7df2f1d08f0682cfbdcfad618602446
KonstantinS343 <mihailrudkovic@gmail.com>
2023-10-09 15:40:54 +0300

    Complete server handlers

--- lab5/server/server.py
+++ lab5/server/server.py
@@ -2,19 +2,22 @@ import socket
 import logging
 import configparser
 import random
+import re
 
 from utils.request.request import Request
 from utils.response.response import Response
+from utils.file_share import FileHandler
 
 
 class HTTPServer:
-    BUFFER_SIZE = 1024
+    BUFFER_SIZE = 4096
 
     def __init__(self) -> None:
         self.__config = configparser.ConfigParser()
         self.__config.read('server.ini')
         self.__host = 'localhost'  # self.__config['server']['host']
         self.__port = random.randint(1, 10000)
+        self.__file = FileHandler()
 
     def run(self):
         server = socket.socket(
@@ -28,7 +31,7 @@ class HTTPServer:
             logging.info(f'Server is running at http://{self.__host}:{self.__port}')
             self.server_loop(server=server)
         except Exception as e:
-            print(e)
+            logging.info(e)
         finally:
             server.close()
 
@@ -36,11 +39,26 @@ class HTTPServer:
         try:
             while True:
                 connection, addr = server.accept()
+
                 logging.info(f'{addr[0]} connected')
 
-                data = connection.recv(HTTPServer.BUFFER_SIZE)
+                data: bytes = connection.recv(400)
+                str_data = data.decode()
+                try:
+                    request_length = re.findall(r'Content-Length: \d+', str_data)[0]
+                except Exception:
+                    pass
+                else:
+                    _, request_length = request_length.split('Content-Length: ')
+                    content_length, request_length = len(data), int(request_length)
+
+                    while content_length < request_length:
+                        read_bytes = connection.recv(HTTPServer.BUFFER_SIZE)
+                        data += read_bytes
+                        content_length += len(read_bytes)
                 request = Request(data)
-                response = Response(headers=request.headers())
+                uploading = self.__file.upload(request.headers())
+                response = Response(request=request, uploading_flag=uploading)
 
                 connection.sendall(response.handle_response())
                 connection.shutdown(socket.SHUT_WR)
@@ -48,4 +66,4 @@ class HTTPServer:
         except KeyboardInterrupt:
             logging.info('Shutting down...')
         except Exception as e:
-            print('Exception', e)
+            logging.info(e)
--- lab5/server/utils/request/request.py
+++ lab5/server/utils/request/request.py
@@ -4,26 +4,42 @@ import re
 
 
 class RequestParser:
-    def handler(self, data: str) -> Dict[str, str]:
-        headers = {}
-        print(data)
-        request = data.splitlines()
-        headers['Request URL'] = re.findall(r'\/[a-zA-Z.\/]*', request[0])[0].strip()
-        headers['Request Method'] = re.findall(r'^([A-Z]+)', request[0])[0]
-        for i in request:
-            if i[:4] == 'Host':
-                headers['Host'] = re.findall(r'[a-zA-Z]+:\d+', i)[0]
-                break
-        logging.info(f'[Request Method] - {headers["Request Method"]}')
-        logging.info(f'[Request URL] - {headers["Request URL"]}')
-        logging.info(f'[Host] - {headers["Host"]}')
-        return headers
+    def __init__(self, data: bytes) -> None:
+        try:
+            headers, file_info, body = data.split(b'\r\n\r\n')
+        except Exception:
+            error_split = True
+            headers, file_info = data.split(b'\r\n\r\n')
+        else:
+            error_split = False
+        request = (headers+file_info).decode().splitlines()
+        self.__request = {'head': request[0]}
+        for i in request[1:]:
+            try:
+                key, value = i.split(': ')
+            except Exception:
+                if key == 'Content-Type':
+                    break
+                continue
+            self.__request[key] = value
+
+        if not error_split:
+            self.__request['body'] = body
+
+    def handler(self) -> Dict[str, str]:
+        self.__request['Request URL'] = re.findall(r'\/[a-zA-Z.\/]*', self.__request['head'])[0].strip()
+        self.__request['Request Method'] = re.findall(r'^([A-Z]+)', self.__request['head'])[0]
+        logging.info(f'[Request Method] - { self.__request["Request Method"]}')
+        logging.info(f'[Request URL] - { self.__request["Request URL"]}')
+        logging.info(f'[Host] - { self.__request["Host"]}')
+        logging.info(f'[Content-Type] - { self.__request["Content-Type"]}')
+        return self.__request
 
 
 class Request:
     def __init__(self, data: bytes) -> None:
-        self.__parser = RequestParser()
-        self.__headers = self.__parser.handler(data.decode())
+        self.__parser = RequestParser(data=data)
+        self.__headers = self.__parser.handler()
 
     def headers(self) -> Dict[str, str]:
         return self.__headers
